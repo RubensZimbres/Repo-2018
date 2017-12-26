9b258952d10207832c5959fc52a28569f19002e9
Rubens Zimbres <rubens.zimbres@eclipso.de>
2017-12-26 15:37:05 -0200

    GAN in Tensorflow

--- Tensorflow/GAN.py
+++ Tensorflow/GAN.py
@@ -20,31 +20,31 @@ x_test=x_test[n:n+n]
 noise_factor = 0.1
 x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape) 
 x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape) 
-y_test_noisy = y_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=y_test.shape) 
-
 
 x_train_noisy = np.clip(x_train_noisy, 0., 1.)
 x_test_noisy = np.clip(x_test_noisy, 0., 1.)
 
 x_train_noisy=np.concatenate([x_train_noisy,x_train_noisy])
+x_test_noisy=np.concatenate([x_test_noisy,x_test_noisy])
 x_train=np.concatenate([x_train,x_train])
 
 np.random.seed(200)
 sel=random.sample(range(0,x_train.shape[0]), n)
-y_train=pd.get_dummies(y_train[sel])
 x_train_noisy=x_train_noisy[sel]
+x_test_noisy=x_test_noisy[sel]
 x_train=x_train[sel]
 
 y_train=y_train[0:n]
 y_train=np.concatenate([y_train,y_train])[sel]
-
+y_train=np.array(pd.get_dummies(y_train)).astype(np.float32)
 y_test=y_test[0:n]
-y_test=np.concatenate([y_test,y_test])[sel]
+y_test0=np.concatenate([y_test,y_test])[sel]
+y_test=np.array(pd.get_dummies(y_test0))
 
 x_train=np.array(x_train).astype(np.float64)
 x_train_noisy=x_train_noisy.astype(np.float64)
 
-learning_rate = 0.008
+learning_rate = 0.002
 batch_size = n
 
 display_step = 50
@@ -94,9 +94,6 @@ y_true = Y
 loss = tf.reduce_mean(tf.pow(y_true - y_pred, 2))
 optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
 
-logits0 = decoder(encoder(X))
-prediction0 = tf.nn.sigmoid(logits0)
-
 
 def next_batch(num, data, labels):
     idx = np.arange(0 , len(data))
@@ -108,25 +105,6 @@ def next_batch(num, data, labels):
 
 
 
-'''np.where(np.array(sel)<100)
-
-
-plt.figure(figsize=(7, 7))
-ax = plt.subplot(1, 2, 1)
-plt.imshow(x_train_noisy[2].reshape(28, 28))
-plt.gray()
-ax.get_xaxis().set_visible(False)
-ax.get_yaxis().set_visible(False)
-ax = plt.subplot(1, 2, 2)
-plt.imshow(np.array(pred0)[0].reshape(100,28, 28)[2])
-plt.gray()
-ax.get_xaxis().set_visible(False)
-ax.get_yaxis().set_visible(False)
-plt.show()
-
-plt.imshow(x_train[0].reshape(28,28))
-plt.imshow(x_train_noisy[0].reshape(28,28))
-''' 
 
 
 ### LINK AUTOENCODER + CNN
@@ -189,20 +167,9 @@ train_op = optimizer2.minimize(loss_op2)
 correct_pred2 = tf.equal(tf.argmax(prediction2, 1), tf.argmax(Y2, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred2, tf.float32))
 
-
-
 init = tf.global_variables_initializer()
 
-
-(x_tt, y_train), (x_tttt, y_test) = mnist.load_data()
-y_train=y_train[0:n]
-y_train=np.concatenate([y_train,y_train])[sel]
-y_train=np.array(pd.get_dummies(y_train)).astype(np.float32)
-y_test=y_test[0:n]
-y_test0=np.concatenate([y_test,y_test])[sel]
-y_test=np.array(pd.get_dummies(y_test0))
-
-num_steps = 2000
+num_steps =4000
 
 
 with tf.Session() as sess:
@@ -228,7 +195,24 @@ with tf.Session() as sess:
     output3=sess.run([decoder_op],feed_dict={X: x_test_noisy})
     x_test22=np.array(output3).reshape(n,784).astype(np.float64)
 
-    output33 = sess.run([prediction2],feed_dict={X2:x_test22,keep_prob: 1})
+    output33 = sess.run([prediction2],feed_dict={X2:x_test22,keep_prob: 1.0})
     pred0=np.argmax(output33,axis=2)[0]
 
 print('Accuracy Test:',1-np.count_nonzero(y_test0-pred0)/n)
+
+np.where(np.array(sel)<100)
+
+
+plt.figure(figsize=(7, 7))
+ax = plt.subplot(1, 2, 1)
+plt.imshow(x_test_noisy[2].reshape(28, 28))
+plt.gray()
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+ax = plt.subplot(1, 2, 2)
+plt.imshow(np.array(output3)[0].reshape(n,28, 28)[2])
+plt.gray()
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+plt.show()
+
