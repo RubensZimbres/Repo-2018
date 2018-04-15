ee0f53553fb5ef46b2318e1a7fb713b4106800ca
Rubens Zimbres <rubenszmm@gmail.com>
2018-04-15 08:23:43 -0300

    Mergevec Python 3

--- OpenCV/Mergevec3.py
+++ OpenCV/Mergevec3.py
@@ -19,44 +19,20 @@ def get_args():
 	return (args.vec_directory, args.output_filename)
 
 def merge_vec_files(vec_directory, output_vec_file):
-	"""
-	Iterates throught the .vec files in a directory and combines them.
-	(1) Iterates through files getting a count of the total images in the .vec files
-	(2) checks that the image sizes in all files are the same
-	The format of a .vec file is:
-	4 bytes denoting number of total images (int)
-	4 bytes denoting size of images (int)
-	2 bytes denoting min value (short)
-	2 bytes denoting max value (short)
-	ex: 	6400 0000 4605 0000 0000 0000
-		hex		6400 0000  	4605 0000 		0000 		0000
-			   	# images  	size of h * w		min		max
-		dec	    	100     	1350			0 		0
-	:type vec_directory: string
-	:param vec_directory: Name of the directory containing .vec files to be combined.
-				Do not end with slash. Ex: '/Users/username/Documents/vec_files'
-	:type output_vec_file: string
-	:param output_vec_file: Name of aggregate .vec file for output.
-		Ex: '/Users/username/Documents/aggregate_vec_file.vec'
-	"""
 
-	# Check that the .vec directory does not end in '/' and if it does, remove it.
 	if vec_directory.endswith('/'):
 		vec_directory = vec_directory[:-1]
-	# Get .vec files
+
 	files = glob.glob('{0}/*.vec'.format(vec_directory))
 
-	# Check to make sure there are .vec files in the directory
 	if len(files) <= 0:
 		print('Vec files to be mereged could not be found from directory: {0}'.format(vec_directory))
 		sys.exit(1)
-	# Check to make sure there are more than one .vec files
 	if len(files) == 1:
 		print('Only 1 vec file was found in directory: {0}. Cannot merge a single file.'.format(vec_directory))
 		sys.exit(1)
 
 
-	# Get the value for the first image size
 	prev_image_size = 0
 	try:
 		with open(files[0], 'rb') as vecfile:
@@ -87,9 +63,6 @@ def merge_vec_files(vec_directory, output_vec_file):
 			print('An IO error occured while processing the file: {0}'.format(f))
 			exception_response(e)
 
-
-	# Iterate through the .vec files, writing their data (not the header) to the output file
-	# '<iihh' means 'little endian, int, int, short, short'
 	header = struct.pack('<iihh', total_num_images, image_size, 0, 0)
 	try:
 		with open(output_vec_file, 'wb') as outputfile:
@@ -111,3 +84,5 @@ if __name__ == '__main__':
 		sys.exit('mergevec requires an output filename. Call mergevec.py with -o your_output_filename')
 
 	merge_vec_files(vec_directory, output_filename)
+	
+Source: https://github.com/wulfebw/mergevec
