c953c328f041d4f38a8638218f23b3b2bacc3117
Rubens Zimbres <rubenszmm@gmail.com>
2018-07-08 08:42:19 -0300

    Update lambda.py

--- AWS API Gateway/lambda.py
+++ AWS API Gateway/lambda.py
@@ -2,5 +2,5 @@ import boto3
 
 def lambda_handler(event, context):
     client = boto3.client('dynamodb')
-    response = client.put_item(TableName="Kron",Item=event)
+    response = client.put_item(TableName="Table2",Item=event)
     return response
