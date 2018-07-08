9f1bfe1ae328ab00c88a9849dc142c43a98ac161
Rubens Zimbres <rubenszmm@gmail.com>
2018-07-07 21:21:22 -0300

    Update post_api_gateway_json.py

--- AWS API Gateway/post_api_gateway_json.py
+++ AWS API Gateway/post_api_gateway_json.py
@@ -1,17 +1,22 @@
 import requests
+import json
+API_URL = "https://abcd12345.execute-api.us-east-1.amazonaws.com/stage"
 
-KERAS_REST_API_URL = "https://abcd12345.execute-api.us-east-1.amazonaws.com/stage"
-
-payload ={"TableName":"Kron","Item":{"type":"22","page":"2"}}
+mess ={"TableName":"Kron","Item":{"type":{"S":"22"},"page":{"S":"2"}}}
 
 headers = {'Content-Type':'application/json',
-           'Authorization':'Abcd12345'}
-
-for i in range(0,3):
+           'Authorization':'abcd12345'}
+loopCount =0
+for i in range(0,8):
     try:
-        rr=requests.post(KERAS_REST_API_URL, data=payload,headers=headers)
+        message = {}
+        message['message'] = mess
+        message['sequence'] = loopCount
+        messageJson = json.dumps(message)
+        rr=requests.post(API_URL, data=messageJson,headers=headers)
         rr
         print(rr)
         print("ok")
+        loopCount += 1
     except:
         print("error")
