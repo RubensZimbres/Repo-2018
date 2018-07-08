b06e8cbbd74db73d23a5bac8d17f0efadb845701
Rubens Zimbres <rubenszmm@gmail.com>
2018-07-07 21:54:38 -0300

    Update post_api_gateway_json.py

--- AWS API Gateway/post_api_gateway_json.py
+++ AWS API Gateway/post_api_gateway_json.py
@@ -1,22 +1,17 @@
 import requests
 import json
-API_URL = "https://abcd12345.execute-api.us-east-1.amazonaws.com/stage"
+API_URL = "https://abcd1234.execute-api.us-east-1.amazonaws.com/stage"
 
-mess ={"TableName":"Kron","Item":{"type":{"S":"22"},"page":{"S":"2"}}}
+message ={"TableName":"Kron","Item":{"type":{"S":"22"},"page":{"S":"2"}}}
 
 headers = {'Content-Type':'application/json',
            'Authorization':'abcd12345'}
-loopCount =0
+
 for i in range(0,8):
     try:
-        message = {}
-        message['message'] = mess
-        message['sequence'] = loopCount
-        messageJson = json.dumps(message)
-        rr=requests.post(API_URL, data=messageJson,headers=headers)
+        rr=requests.post(API_URL, data=json.dumps(message),headers=headers)
         rr
         print(rr)
         print("ok")
-        loopCount += 1
     except:
         print("error")
