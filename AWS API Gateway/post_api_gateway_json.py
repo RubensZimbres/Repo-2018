6bf4728be69c2950824079ac50b5eaeef494abf2
Rubens Zimbres <rubenszmm@gmail.com>
2018-07-10 11:20:48 -0300

    Update post_api_gateway_json.py

--- AWS API Gateway/post_api_gateway_json.py
+++ AWS API Gateway/post_api_gateway_json.py
@@ -1,14 +1,15 @@
 import requests
 import json
-API_URL = "https://abcd1234.execute-api.us-east-1.amazonaws.com/stage"
+import random
+API_URL = "https://gycm7vglz2.execute-api.us-east-1.amazonaws.com/Stage0"
 
-# dynamic ID
-message =message ={ "MessageID" : { "S" : "12345" }, 
+# Dynamic ID
+message ={ "MessageID" : { "S" : str(random.randint(0,999999)) }, 
 "type" : { "S" : "type" }, 
 "page" : { "S" : "page"}}
 
-headers = {'content-type':'application/json',
-           'X-Api-Key':'abcd12345'}
+headers = {'Content-Type':'application/json',
+           'X-Api-Key':'A6rKr9oBPX6LNnkH1AiLg1JqjMAVOOsT4IBoE4Fk'}
 
 for i in range(0,8):
     try:
@@ -17,4 +18,4 @@ for i in range(0,8):
         print(rr)
         print("ok")
     except:
-        print("error")
+        print("error")    
