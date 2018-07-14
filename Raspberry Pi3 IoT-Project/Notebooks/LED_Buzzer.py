c1dd1a465eb957582fd22d9fdeea68445fbb17fb
Rubens Zimbres <rubenszmm@gmail.com>
2018-07-14 15:04:27 -0300

    Update LED_Buzzer.py

--- Raspberry Pi3 IoT-Project/Notebooks/LED_Buzzer.py
+++ Raspberry Pi3 IoT-Project/Notebooks/LED_Buzzer.py
@@ -3,10 +3,10 @@ from time import sleep
 
 GPIO.setwarnings(False) 
 GPIO.setmode(GPIO.BOARD) 
-GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) 
+GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW) 
 
 while True: 
-    GPIO.output(8, GPIO.HIGH) 
+    GPIO.output(7, GPIO.HIGH) 
     sleep(1) 
-    GPIO.output(8, GPIO.LOW) 
+    GPIO.output(7, GPIO.LOW) 
     sleep(1) 
