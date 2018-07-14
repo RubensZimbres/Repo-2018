8e707a419edbb5a192b75fc111a2dcc3d283854b
Rubens Zimbres <rubenszmm@gmail.com>
2018-07-14 17:48:15 -0300

    Update BigSound_LED_Laser.py

--- Raspberry Pi3 IoT-Project/Notebooks/BigSound_LED_Laser.py
+++ Raspberry Pi3 IoT-Project/Notebooks/BigSound_LED_Laser.py
@@ -5,12 +5,12 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)
 
-GPIO.setup(8,GPIO.IN)
+GPIO.setup(11,GPIO.IN)
 while True:
-    if GPIO.input(8)==GPIO.LOW:
+    print(GPIO.input(11))
+    if GPIO.input(11)==GPIO.HIGH:
         GPIO.output(7, GPIO.HIGH)
         sleep(1)
     else:
         sleep(1)
         GPIO.output(7, GPIO.LOW)
-    
