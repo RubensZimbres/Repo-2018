b44b92619d199c93fb376579240c4ffc22400bf6
Rubens Zimbres <rubenszmm@gmail.com>
2018-07-16 15:25:19 -0300

    Create Touch_LED_Buzzer.py

---
+++ Raspberry Pi3 IoT-Project/Notebooks/Touch_LED_Buzzer.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)
+
+GPIO.setup(5,GPIO.IN)
+while True:
+    print(GPIO.input(5))
+    if GPIO.input(5)==GPIO.HIGH:
+        GPIO.output(7, GPIO.HIGH)
+        sleep(1)
+    else:
+        sleep(1)
+        GPIO.output(7, GPIO.LOW)
