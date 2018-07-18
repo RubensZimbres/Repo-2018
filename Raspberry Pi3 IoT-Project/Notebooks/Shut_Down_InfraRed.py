53fb34f8cce5f6028d7014a992ae6224e4a2d9a6
Rubens Zimbres <rubenszmm@gmail.com>
2018-07-18 12:34:18 -0300

    Update Shut_Down_InfraRed.py

--- Raspberry Pi3 IoT-Project/Notebooks/Shut_Down_InfraRed.py
+++ Raspberry Pi3 IoT-Project/Notebooks/Shut_Down_InfraRed.py
@@ -1,7 +1,7 @@
 import os
 import RPi.GPIO as GPIO
 from time import sleep
-
+import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)
@@ -9,10 +9,15 @@ GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(5,GPIO.IN)
 while True:
     print(GPIO.input(5))
-    if GPIO.input(5)==GPIO.HIGH:
+    if GPIO.input(5)==GPIO.LOW:
         GPIO.output(7, GPIO.HIGH)
         sleep(1)
-        os.system("sudo shutdown now -h")
+
+        time.sleep(8)
+
+        os.system("exit && sudo poweroff -f")
     else:
         sleep(1)
+
         GPIO.output(7, GPIO.LOW)
+
