6ed8ff1ac107c97ac7f99acd788ffe992ecc24ef
Rubens Zimbres <rubenszmm@gmail.com>
2018-08-07 16:40:15 -0300

    Create Spacy.py

---
+++ NLP/Spacy.py
@@ -0,0 +1,31 @@
+from gensim import corpora
+documents = ["Interface máquina humana para aplicações computacionais de laboratório abc",
+              "Um levantamento da opinião do usuário sobre o tempo de resposta do sistema informático",
+               "O sistema de gerenciamento de interface do usuário EPS",
+               "Sistema e testes de engenharia de sistemas humanos de EPS",
+               "Relação do tempo de resposta percebido pelo usuário para a medição de erro",
+               "A geração de árvores não ordenadas binárias aleatórias",
+               "O gráfico de interseção dos caminhos nas árvores personalizadas",
+               "Gráfico de menores IV Largura de árvores personalizadas e bem quase encomendado",
+               "Gráficos menores Uma pesquisa"]
+
+
+stoplist = set('a e o para sobre'.split())
+texts = [[word for word in document.lower().split() if word not in stoplist]
+    for document in documents]
+texts=[[x.lower() for x in texts[i]] for i in range(0,9)]
+
+long_words1 = [[w for w in texts[i] if len(w)>3] for i in range(0,9)]
+
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+
+import spacy
+from spacy.lang.pt.examples import sentences
+
+!python -m spacy download pt_core_news_sm
+
+nlp = spacy.load('pt_core_news_sm')
+
+long_words1=[[nlp(str(i)) for i in documents[k].lower().split() if len(i)>3] for k in range(0,9)] 
+
