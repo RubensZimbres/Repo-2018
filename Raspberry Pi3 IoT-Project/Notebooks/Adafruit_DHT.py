3408e4e74a9dfa7655f87c0f34c74476e94e5ca7
Rubens Zimbres <rubenszmm@gmail.com>
2018-08-23 13:40:44 -0300

    Update Adafruit_DHT.py

--- Raspberry Pi3 IoT-Project/Notebooks/Adafruit_DHT.py
+++ Raspberry Pi3 IoT-Project/Notebooks/Adafruit_DHT.py
@@ -11,8 +11,7 @@ if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
     sensor = sensor_args[sys.argv[1]]
     pin = sys.argv[2]
 else:
-    print('usage: sudo ./Adafruit_DHT.py [11|22|2302] GPIOpin#')
-    print('example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO #4')
+    pass
     sys.exit(1)
 
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
@@ -22,5 +21,5 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 if humidity is not None and temperature is not None:
     print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
 else:
-    print('Failed to get reading. Try again!')
+    print('Failed')
     sys.exit(1)
