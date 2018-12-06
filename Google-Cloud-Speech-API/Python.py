0f86074e938d1be331e73c04c82c13ebfccc7782
Rubens Zimbres <rubenszmm@gmail.com>
2018-12-06 10:04:13 -0200

    Update Python.py

--- Google-Cloud-Speech-API/Python.py
+++ Google-Cloud-Speech-API/Python.py
@@ -1,4 +1,7 @@
-## ENVIRONMENT VARIABLE -- GOOGLE_APPLICATION_CREDENTIALS="D:\VM\Google Cloud\Speech_API.json"
+## ENVIRONMENT VARIABLE -- GOOGLE_APPLICATION_CREDENTIALS="D:\VM\Google Cloud\Speech_API_123456789.json"
+
+import time
+start=time.time()
 
 import io
 import os
@@ -7,23 +10,45 @@ from google.cloud import speech
 from google.cloud.speech import enums
 from google.cloud.speech import types
 
+#! pip install --upgrade google-cloud-storage
+#! pip install webapp2
+#! pip install cloudstorage
+#! pip install GoogleAppEngineCloudStorageClient
+
+from google.cloud import storage
+
+client = storage.Client()
+
+bucket = client.get_bucket('storagexxx')
+
+blob = bucket.get_blob('Info/1-NotSolved_No_Silence.wav')
+
+blob3=blob.upload_from_filename(filename='1-NotSolved_No_Silence.wav')  
+
+
+audio = types.RecognitionAudio(content=blob3)
+
+
 client = speech.SpeechClient()
 
-file_name = os.path.join(
-    os.path.dirname(__file__),
-    'resources',
-    'audio.raw')
-
-with io.open(file_name, 'rb') as audio_file:
-    content = audio_file.read()
-    audio = types.RecognitionAudio(content=content)
-
 config = types.RecognitionConfig(
-    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-    sample_rate_hertz=16000,
-    language_code='en-US')
+    encoding=enums.RecognitionConfig.AudioEncoding.MULAW,
+    sample_rate_hertz=8000,enable_word_time_offsets= False,
+    language_code='pt-BR',
+    enable_automatic_punctuation= False,
+    use_enhanced=True)
 
-response = client.recognize(config, audio)
+audio=types.RecognitionAudio(uri="gs://storagexxx/Info/1-NotSolved_No_Silence.wav")
+
+operation = client.long_running_recognize(config, audio)
+
+print('Waiting for operation to complete...')
+response = operation.result(timeout=90)
+response
 
 for result in response.results:
-    print('Transcript: {}'.format(result.alternatives[0].transcript))
+        print(u'Transcript: {}'.format(result.alternatives[0].transcript))
+        print('Confidence: {}'.format(result.alternatives[0].confidence))
+end=time.time()
+
+print((end-start)/60,'minutos')
