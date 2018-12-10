3b50a1d64aaf4bc2a740da3b0a6bcbd36212a363
Rubens Zimbres <rubenszmm@gmail.com>
2018-12-10 12:09:55 -0200

    Create Python_request.py

---
+++ AWS-Comprehend/Python_request.py
@@ -0,0 +1,9 @@
+import boto3
+import json
+
+comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
+text = "Damn it! It is raining today in Los Angeles!"
+
+print('Calling DetectEntities')
+print(json.dumps(comprehend.detect_entities(Text=text, LanguageCode='en'), sort_keys=True, indent=4))
+print(json.dumps(comprehend.detect_sentiment(Text=text, LanguageCode='en'), sort_keys=True, indent=4))
