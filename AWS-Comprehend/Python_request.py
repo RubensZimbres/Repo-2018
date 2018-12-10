61b809db2c461d852fbf63ade0e357bdd0eb8551
Rubens Zimbres <rubenszmm@gmail.com>
2018-12-10 15:12:58 -0200

    Update Python_request.py

--- AWS-Comprehend/Python_request.py
+++ AWS-Comprehend/Python_request.py
@@ -2,7 +2,7 @@ import boto3
 import json
 
 comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
-text = "Damn it! It is raining today in Los Angeles!"
+text = "Damn it! It is raining 30 milimeters today in Los Angeles!"
 
 print('Calling DetectEntities')
 print(json.dumps(comprehend.detect_entities(Text=text, LanguageCode='en'), sort_keys=True, indent=4))
