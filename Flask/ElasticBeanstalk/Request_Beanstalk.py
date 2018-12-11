b783ca43205a9bc8b170f6d91570bcf46720ac1a
Rubens Zimbres <rubenszmm@gmail.com>
2018-12-11 13:12:28 -0200

    Create Request_Beanstalk.py

---
+++ Flask/ElasticBeanstalk/Request_Beanstalk.py
@@ -0,0 +1,25 @@
+import requests
+import boto3
+from io import BytesIO
+import matplotlib.image as mpimg
+
+client = boto3.client('s3')
+
+resource = boto3.resource('s3')
+my_bucket = resource.Bucket('bucket-vecto')
+
+image_object = my_bucket.Object('dog.jpg')
+image = mpimg.imread(BytesIO(image_object.get()['Body'].read()), 'jpg')
+
+KERAS_REST_API_URL = "http://localhost:5000/predict"
+
+payload = {"image": image}
+
+r = requests.post(KERAS_REST_API_URL, files=payload).json()
+
+if r["success"]:
+    for (i, result) in enumerate(r["predictions"]):
+        print("{}. {}: {:.4f}".format(i + 1, result["label"],
+            result["probability"]))
+else:
+    print("Request failed")
