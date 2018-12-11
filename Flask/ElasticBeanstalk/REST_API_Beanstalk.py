e7148937c9b93bbf46eeaa8370c58ff6f23c23a5
Rubens Zimbres <rubenszmm@gmail.com>
2018-12-11 13:32:48 -0200

    Update REST_API_Beanstalk.py

--- Flask/ElasticBeanstalk/REST_API_Beanstalk.py
+++ Flask/ElasticBeanstalk/REST_API_Beanstalk.py
@@ -44,4 +44,5 @@ if __name__ == "__main__":
     print(("* Loading Keras model and Flask starting server..."
         "please wait until server has fully started"))
     load_model()
+    app.debug = True
     app.run()
