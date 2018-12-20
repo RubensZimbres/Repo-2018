f400ef59012277a96c9d2abe2e414b91df1eb59a
Rubens Zimbres <rubenszmm@gmail.com>
2018-12-20 17:40:03 -0200

    Create hyperopt.py

---
+++ Hyperopt/hyperopt.py
@@ -0,0 +1,29 @@
+from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
+
+
+def accuracy(params):
+    clf = RandomForestClassifier(**params)
+    return cross_val_score(clf, x, y).mean()
+
+parameters = {
+    'max_depth': hp.choice('max_depth', range(30,60)),
+    'max_features': hp.choice('max_features', range(3,4)),
+    'n_estimators': hp.choice('n_estimators', range(100,150)),
+    "max_leaf_nodes":hp.choice("max_leaf_nodes",range(3,10)),
+    "min_samples_leaf":hp.choice("min_samples_leaf",range(1,40)),
+    "min_samples_split":hp.choice("min_samples_split",range(2,40)),
+    'criterion': hp.choice('criterion', ["gini", "entropy"])}
+
+
+best = 0
+def f(params):
+    global best
+    acc = accuracy(params)
+    if acc > best:
+        best = acc
+    print ('Improving:', best, params)
+    return {'loss': -acc, 'status': STATUS_OK}
+
+trials = Trials()
+best = fmin(f, parameters, algo=tpe.suggest, max_evals=100, trials=trials)
+print ('best:',best)
