7cf4ca5a79869ede27caa01dc4668b850cea8333
Rubens Zimbres <rubenszmm@gmail.com>
2018-12-27 08:33:18 -0200

    Create Python.py

---
+++ AWS-Transcribe/Python.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+import time
+import boto3
+
+transcribe = boto3.client('transcribe')
+job_name = "Name"
+job_uri = "https://s3.amazonaws.com/bucket/Inform_Clean.wav"
+
+transcribe.start_transcription_job(
+    TranscriptionJobName=job_name,
+    Media={'MediaFileUri': job_uri},
+    MediaFormat='wav',
+    LanguageCode='pt-BR')
+
+while True:
+    status = transcribe.get_transcription_job(TranscriptionJobName=job_name)
+    if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
+        break
+	print('Not ready yet')
+    time.sleep(5)
+    
+print(status)
