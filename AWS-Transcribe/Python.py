c01e2dcde08ecb1751094b3c88ca37731197f6e2
Rubens Zimbres <rubenszmm@gmail.com>
2018-12-27 10:08:11 -0200

    Update Python.py

--- AWS-Transcribe/Python.py
+++ AWS-Transcribe/Python.py
@@ -1,16 +1,18 @@
 from __future__ import print_function
 import time
 import boto3
-
 transcribe = boto3.client('transcribe')
-job_name = "Name"
-job_uri = "https://s3.amazonaws.com/bucket/Inform_Clean.wav"
-
+job_name = "Name4"
+job_uri = "https://s3.amazonaws.com/bucket/Inform_Clean2.wav"
 transcribe.start_transcription_job(
     TranscriptionJobName=job_name,
     Media={'MediaFileUri': job_uri},
     MediaFormat='wav',
-    LanguageCode='pt-BR')
+    LanguageCode='pt-BR',
+    OutputBucketName= "cdf-ok",
+    MediaSampleRateHertz=8000,
+    Settings={"MaxSpeakerLabels": 2,
+      "ShowSpeakerLabels": True})
 
 while True:
     status = transcribe.get_transcription_job(TranscriptionJobName=job_name)
@@ -19,3 +21,12 @@ while True:
     print('Not ready yet')
     time.sleep(15)
 print(status)
+
+import json
+
+s3=boto3.client('s3')
+
+obj = s3.get_object(Bucket='cdf-ok', Key=job_name+'.json')
+jj = json.loads(obj['Body'].read())
+
+print(jj)
