3b869b3e204c74e232d0f095e4d4242da1b80888
Rubens Zimbres <rubenszmm@gmail.com>
2019-01-03 09:06:42 -0200

    Update Aggregate.py

--- Pandas/Aggregate.py
+++ Pandas/Aggregate.py
@@ -1,5 +1,11 @@
 df.set_index('Data', inplace=True)
 
-df2=df.groupby([df.Serial,df.index.month,df.index.day]).sum()
+df2=df.groupby([df.Serial,df.index.year,df.index.month,df.index.day]).sum()
 
 df3=df2.reset_index(level=['Serial'])
+
+df3.index.names=['Year','Month','Day']
+
+df4=df3.reset_index()
+
+df4['Date'] = pd.to_datetime(df4[['Year','Month','Day']])
